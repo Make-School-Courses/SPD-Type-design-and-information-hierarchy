5ea5c0e80b6bbc8e9258cd692b4cd4294082aa20
Mitchell <soggybag@gmail.com>
2018-11-14 14:05:52 -0800

    Updated Exercise

--- exercise/Type-Hierarchy-XCode/Type-Hierarchy-XCode/AppDelegate.swift
+++ exercise/Type-Hierarchy-XCode/Type-Hierarchy-XCode/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  Type-Hierarchy
+//  Type-Hierarchy-XCode
 //
-//  Created by mitchell hudson on 11/13/16.
-//  Copyright © 2016 Mitchell Hudson. All rights reserved.
+//  Created by Mitchell Hudson on 11/14/18.
+//  Copyright © 2018 Mitchell Hudson. All rights reserved.
 //
 
 import UIKit
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
